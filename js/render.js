f729f18214017e09e94f7dbfa8c1fb03cecdd1c3
Sebastian Ebel <sebastianebel@hotmail.de>
2021-08-11 20:56:34 +0200

    About almost added

--- js/render.js
+++ js/render.js
@@ -10,7 +10,28 @@ async function loadPokemon() {
     renderPokemon();
 }
 
-function renderPokemon() {
-    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
+function renderPokemon(currentPokemons) {
+    renderHeader();
+    
+    
+    document.getElementById('species-name').innerHTML = currentPokemon['species']['name'];
+    document.getElementById('species-height').innerHTML = currentPokemon['height'];
+    document.getElementById('species-weight').innerHTML = currentPokemon['weight'];
+
+    for (let i = 0; i < currentPokemon['abilities'].length; i++) {
+        const ability = currentPokemon['abilities'][i];
+            document.getElementById('species-abilities').innerHTML += `${ability['ability']['name']},  `;
+    }
+}
+
+function renderHeader() {
     document.getElementById('pokemon-pic').src = currentPokemon['sprites']['front_default'];
+    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'];
+    currentPokemons = document.getElementById('pokemon-index');
+    currentPokemons.innerHTML += `#00${currentPokemon['id']}`; 
+    
+    for (let j = 0; j < currentPokemon['types'].length; j++) {
+        const speciestype = currentPokemon['types'][j];
+        document.getElementById('pokemon-type').innerHTML += `${speciestype['type']['name']}`;
+    }
 }
